7f33468a209507c1bc8d5bad8aea0f74de571fe1
Anthony Bigotte <bigotte.anthony@gmail.com>
2023-03-20 10:56:06 +0100

    added search bar to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export default function Header() {
   return (
@@ -18,6 +19,19 @@ export default function Header() {
           alt='Instagram'
           fill='true'
           className='object-contain'
+        />
+      </div>
+
+      <div className='relative mt-1'>
+        <div className='absolute top-2 left-2'>
+          <AiOutlineSearch className='text-2xl text-gray-500' />
+        </div>
+        <input
+          type='text'
+          name='search'
+          id='search'
+          placeholder='Search'
+          className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'
         />
       </div>
     </header>
