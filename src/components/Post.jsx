896e96df4a55a55c59b40a099279586686dfac0f
Anthony Bigotte <bigotte.anthony@gmail.com>
2023-03-21 10:52:31 +0100

    added caption and input box

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BsThreeDots, BsBookmark } from 'react-icons/bs';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { FaRegCommentDots } from 'react-icons/fa';
+import { GrEmoji } from 'react-icons/gr';
 
 export default function Post({ img, userImg, caption, username, id }) {
   return (
     <div className=''>
+      {/* Header */}
       <div className='flex items-center p-5 justify-between bg-white my-7'>
         <div className='flex items-center'>
           <img
@@ -17,7 +19,9 @@ export default function Post({ img, userImg, caption, username, id }) {
         </div>
         <BsThreeDots className='h-5' />
       </div>
+      {/* Image */}
       <img className='object-cover w-full' src={img} alt={caption} />
+      {/* Buttons */}
       <div className='flex justify-between px-4 pt-4'>
         <div className='flex space-x-4'>
           <AiOutlineHeart className='postBtn' />
@@ -25,6 +29,22 @@ export default function Post({ img, userImg, caption, username, id }) {
         </div>
         <BsBookmark className='postBtn' />
       </div>
+
+      {/* Post Comments */}
+      <p className='p-5 truncate'>
+        <span className='font-bold mr-2'>{username}</span>
+        {caption}
+      </p>
+      {/* Post Input Box */}
+      <form className='flex items-center p-4'>
+        <GrEmoji className='text-2xl' />
+        <input
+          type='text'
+          placeholder='Enter your comment...'
+          className='border-none flex-1 focus:ring-0'
+        />
+        <button className='text-blue-400 font-bold'>Post</button>
+      </form>
     </div>
   );
 }
